19afaab5edf05cc14c2b7421899da296fca9841b
Isty-Rachmi <istyrachmi@gmail.com>
2018-04-01 19:49:57 +0700

    command

---
+++ app/src/main/java/com/example/istyrachmiw/istyr_1202154299_modul6/DBcomment.java
@@ -0,0 +1,35 @@
+package com.example.istyrachmiw.istyr_1202154299_modul6;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+//Enkapsulasi data comment
+@IgnoreExtraProperties
+public class DBcomment {
+    String yangkomen, komennya, fotoyangdikomen;
+
+    //Dibutuhkan kosong untuk membaca data
+    public DBcomment(){
+
+    }
+
+    //Constructor dari class ini
+    public DBcomment(String yangkomen, String komennya, String fotoyangdikomen) {
+        this.yangkomen = yangkomen;
+        this.komennya = komennya;
+        this.fotoyangdikomen = fotoyangdikomen;
+    }
+
+    //Sisany getter untuk variabel dari class ini
+    public String getFotoyangdikomen() {
+        return fotoyangdikomen;
+    }
+
+    public String getYangkomen() {
+        return yangkomen;
+    }
+
+    public String getKomennya() {
+        return komennya;
+    }
+
+}
